5b372decb6b3acd90067c3e13766d153f2cd310d
haniii294 <148616894+haniii294@users.noreply.github.com>
2025-04-18 18:34:25 +0700

    Update konfigurasi database yang sudah terhubung dengan database NoSQL menggunakan MongoDB Compass

--- backend/config/db2.js
+++ backend/config/db2.js
@@ -1,1 +1,18 @@
-Update konfigurasi yang sudah terhubung dengan database NoSQL menggunakan MongoDB Compass
+const mongoose = require('mongoose');
+
+// Ganti URI sesuai koneksi MongoDB kamu
+const uri = 'mongodb://localhost:27017/worshipfinder_kel4'; // bisa juga pakai nama database lain
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Koneksi ke MongoDB berhasil!');
+})
+.catch((err) => {
+  console.error('Koneksi MongoDB gagal:', err);
+});
+
+module.exports = mongoose;
+
