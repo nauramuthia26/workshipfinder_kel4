caa08bbaf874b76eb620dbb9a6a2581fd516d30a
Firdafahria23 <150696359+firdafahria@users.noreply.github.com>
2025-04-18 21:05:17 +0700

    db.js
    menambahkan tabel kontak

--- backend/config/db.js
+++ backend/config/db.js
@@ -136,6 +136,14 @@ db.connect((err) => {
     );
 `;
 
+    const creatkontakTable = '
+       CREATE TABLE `kontak` (
+        `nama` varchar(50) NOT NULL,
+        `email` varchar(50) NOT NULL,
+        `pesan` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+        ); 
+';
+    
     // Eksekusi query untuk membuat tabel
     db.query(createUsersTable, (err, result) => {
         if (err) {
